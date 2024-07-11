b36e95a751692a8859bbc658077c28722c7e7215
So-Emily <esoriano110@gmail.com>
2024-07-11 00:42:10 -0700

    worked on media queries. Phone layout is a lot better.

--- js/script.js
+++ js/script.js
@@ -13,5 +13,20 @@ topFunction = () => {
     document.documentElement.scrollTop = 0;
 }
 
+// Tech Stack Sccroll
+window.onload = function() {
+    setTimeout(function() {
+        var scrollContainer = document.querySelector('.tab-pane .row');
+        if (scrollContainer) {
+            console.log('Before reset:', scrollContainer.scrollLeft);
+            scrollContainer.scrollLeft = -5;
+            console.log('After reset:', scrollContainer.scrollLeft);
+            console.log('Container offsetWidth:', scrollContainer.offsetWidth);
+            console.log('Container scrollWidth:', scrollContainer.scrollWidth);
+        }
+    }, 100);
+};
 
 
+console.log("Hello! Thanks for checking out my portfolio! Feel free to reach out to me on any of my socials or through the contact form above. Have a great day! 😊");
+    
